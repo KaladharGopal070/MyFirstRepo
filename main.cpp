aafed2622fee9619b7e85171054f92bde90314f1
KaladharGopal070 <starkindustries070@gmail.com>
2024-05-29 22:41:44 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+
+// "THis is a comment"
+#include<iostream>
+using namespace std;
+int main(){
+    int x;
+    cout << "Congratulations!! YOu have created your first github account"<< endl;
+    cin >> x;
+    cout << "Input was: " << x << endl;
+    return 0;
+
+}
